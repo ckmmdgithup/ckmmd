409cbfa5691405ffcee6d51e6ed2e892d058ab0d
ckmmd <904133983@qq.com>
2020-05-07 11:10:35 +0800

    再完善

--- config/database.php
+++ config/database.php
@@ -23,9 +23,9 @@ return [
             // 服务器地址
             'hostname'          => env('database.hostname', '127.0.0.1'),
             // 数据库名
-            'database'          => env('database.database', 'tp'),
+            'database'          => env('database.database', 'ckmmd'),
             // 用户名
-            'username'          => env('database.username', 'tp'),
+            'username'          => env('database.username', 'ckmmd'),
             // 密码
             'password'          => env('database.password', '904133983'),
             // 端口
